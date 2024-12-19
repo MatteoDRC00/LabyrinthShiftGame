21890bf85a7f085a3511c96cbf16ab4af883845b
MatteoDRC00 <matteo00drc@gmail.com>
2024-12-19 12:27:10 +0100

    first commit

---
+++ entity/GameMap.java
@@ -0,0 +1,24 @@
+public class GameMap {
+
+    //private Tile[][] map;
+    private DifficultyType difficulty;
+    private Integer dimensione;
+    private Integer num_percorsi;
+    private Integer num_ostacoli;
+    private Integer num_nemici;
+
+    /**
+     *
+     * @param difficolt�
+     */
+    public GameMap(DifficultyType difficulty) {
+        // TODO - implement GameMap.GameMap
+        throw new UnsupportedOperationException();
+    }
+
+    public GameMap generateMaze() {
+        // TODO - implement GameMap.generateMaze
+        throw new UnsupportedOperationException();
+    }
+
+}
